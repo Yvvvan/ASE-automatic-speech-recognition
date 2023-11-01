74c57d368acb8a04d11dd448f8bee4040d32c9ed
henrypoppe <henry.poppe@campus.tu-berlin.de>
2023-11-01 15:04:22 +0100

    updated functions

--- recognizer/tools.py
+++ recognizer/tools.py
@@ -1,21 +1,16 @@
-# import...
+import math
 
 def sec_to_samples(x, sampling_rate):
     return x * sampling_rate
 
 
 def next_pow2(x):
-    p = 0
-    while True:
-        if x > 2 ** p:
-            p += 1
-        else:
-            return p
+    return math.ceil(math.log(x, 2))
 
-
-def next_pow2_samples(x, sampling_rate):
-    # TODO implement this method
-    pass
+def dft_window_size(x, sampling_rate):
+    x_len = sec_to_samples(x, sampling_rate)
+    p = next_pow2(x_len)
+    return 2 ** p
 
 
 def get_num_frames(signal_length_samples, window_size_samples, hop_size_samples):
