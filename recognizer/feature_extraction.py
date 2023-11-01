4201b1c2f65f9d28a3dfbc77ba3e0a98ac8f2163
Yufan <yufan.dong@campus.tu-berlin.de>
2023-11-01 17:00:12 +0100

    upload for aufgabe 1

--- recognizer/feature_extraction.py
+++ recognizer/feature_extraction.py
@@ -1,6 +1,20 @@
 # import...
 # z.B.: import recognizer.tools as tools
+import numpy as np
+from scipy.io import wavfile
+
 
 def make_frames(audio_data, sampling_rate, window_size, hop_size):
-    # TODO implement this method
-    pass
+    # audio_data np.array
+    xs = []
+    start = 0
+    while start < len(audio_data):
+        end = int (start + window_size * sampling_rate)
+        if end > len(audio_data):
+            n = end - len(audio_data)
+            xs.append(np.pad(audio_data[start:],(0,n)) * np.hamming(window_size*sampling_rate))
+        else:
+            xs.append(audio_data[start:end]* np.hamming(window_size*sampling_rate))
+        start = int (start + hop_size * sampling_rate)
+
+    return xs
--- uebung1.py
+++ uebung1.py
@@ -1,10 +1,20 @@
 # import...
 # z.B.: import recognizer.feature_extraction as fe
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from recognizer import feature_extraction
+import matplotlib
 
 def compute_features():
     audio_file = 'data/TEST-MAN-AH-3O33951A.wav'
-    
-    # TODO read in audio_file and call make_frames()
+    sampling_rate, signal = wavfile.read(audio_file)
+    xs = feature_extraction.make_frames(signal, sampling_rate, 0.25, 0.1)
+    print(len(xs))
+    '''plt.plot(range(len(xs[0])), xs[0])
+    plt.show()'''
+
+
 
 if __name__ == "__main__":
     compute_features()
+
